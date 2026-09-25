4b631343a3d780ae4f2d863a40844e5e2a77e8ea
LaszloV79 <vajda.laszlo@gmail.com>
2026-09-25 12:33:30 +0200

    masodik verzio

--- elso.py
+++ elso.py
@@ -1,5 +1,21 @@
-felhasznalo_neve = 'Vali'
-felhasznalo_kora = 25
-felhasznalo_kora += 1
-print('Szia',felhasznalo_neve,'!', felhasznalo_kora)
+# ez az első konzultáció
+from json.encoder import encode_basestring_ascii
 
+felhasznalo_neve = 'Valaki'
+felhasznalo_kora = 20 # int(input('Hány éves vagy: '))
+felhasznalo_kora /= 2
+# felhasznalo_neve *= 2
+jegyek = [3, 4, 5]
+jegyek += [1]
+hallgato = {'Név': 'Józsi', 'Kor': 50}
+print('Szia','dr.' + felhasznalo_neve,'!', felhasznalo_kora)
+print(f'Szia dr. {felhasznalo_neve}! Kora: {felhasznalo_kora}')
+print('Szia','dr.' + felhasznalo_neve,'!', felhasznalo_kora, sep='-', end='\n\n')
+
+print(hallgato['Név'])
+print(jegyek)
+
+print('szöveg'.rjust(50, '.'))
+print('szöveg'.ljust(50, '.'))
+print('szöveg'.center(50, '.'))
+print(str(felhasznalo_kora).center(50, '.'))
